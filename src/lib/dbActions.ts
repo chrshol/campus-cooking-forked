77466d2632c0df6831b59b6f0608fc6de7fd785f
chrshol <chrkissi@gmail.com>
2024-11-29 15:26:04 -1000

    Adding createUser function in dbActions.ts

--- src/lib/dbActions.ts
+++ src/lib/dbActions.ts
@@ -1,33 +1,23 @@
 'use server';
 
-import { Hobby, Level, Major } from '@prisma/client';
-import { ICreateStudentForm } from '@/lib/validationSchemas';
 import { prisma } from '@/lib/prisma';
+import { hash } from 'bcrypt';
 
-// eslint-disable-next-line import/prefer-default-export
-export const upsertStudent = async (studentFormData: ICreateStudentForm) => {
-  const studentData = {
-    bio: studentFormData.bio,
-    email: studentFormData.email,
-    level: studentFormData.level as Level,
-    gpa: studentFormData.gpa,
-    hobbies: studentFormData.hobbies as Hobby[],
-    name: studentFormData.name,
-    major: studentFormData.major as Major,
-  };
-  const enrollmentData = {
-    email: studentFormData.email,
-    enrolled: studentFormData.enrolled!,
-  };
-  const student = await prisma.studentData.upsert({
-    where: { email: studentFormData.email },
-    update: studentData,
-    create: studentData,
+
+/**
+ * Creates a new user in the database.
+ * @param credentials, an object with the following properties: email, password.
+ */
+export async function createUser(credentials: { firstName: string; lastName: string; email: string; password: string }) {
+  // console.log(`createUser data: ${JSON.stringify(credentials, null, 2)}`);
+  const password = await hash(credentials.password, 10);
+  await prisma.user.create({
+    data: {
+      firstName: credentials.firstName,
+      lastName: credentials.lastName,
+      email: credentials.email,
+      password,
+    },
   });
-  const enrollment = await prisma.enrollmentData.upsert({
-    where: { email: enrollmentData.email },
-    update: enrollmentData,
-    create: enrollmentData,
-  });
-  return { student, enrollment };
-};
+}
+
