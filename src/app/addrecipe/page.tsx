01d09a78b2321b6fb72b4c3cb58932a81cf7095c
chrshol <chrkissi@gmail.com>
2024-12-01 16:10:21 -1000

    Protect addrecipe>page.tsx , only for users

--- src/app/addrecipe/page.tsx
+++ src/app/addrecipe/page.tsx
@@ -3,8 +3,28 @@ import LoginPage from '@/components/AddRecipe';
 import Footer from '@/components/Footer';
 import './addrecipepage.css';
 import AddRecipeForm from '@/components/AddRecipe';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { loggedInProtectedPage } from '@/lib/page-protection';
+import { getServerSession } from 'next-auth';
 
-export default function AddRecipe() {
+// export default function AddRecipe() {
+//   return (
+//     <div className="min-h-screen bg-white">
+//       <NavBar />
+//       <AddRecipeForm />
+//       <Footer />
+//     </div>
+//   );
+// }
+
+const AddRecipe = async () => {
+  // Protect the page, only logged in users can access it.
+  const session = await getServerSession(authOptions);
+  loggedInProtectedPage(
+    session as {
+      user: { email: string; id: string; randomKey: string };
+    } | null,
+  );
   return (
     <div className="min-h-screen bg-white">
       <NavBar />
@@ -12,4 +32,6 @@ export default function AddRecipe() {
       <Footer />
     </div>
   );
-}
+};
+
+export default AddRecipe;
