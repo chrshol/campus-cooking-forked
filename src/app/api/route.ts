c61fee87c0ca80ecd832de380989ebf23a550995
chrshol <chrkissi@gmail.com>
2024-12-03 19:26:56 -1000

    Update route.ts

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,8 +1,8 @@
-import { getServerSession } from 'next-auth/next';
+import { getServerSession } from 'next-auth';
 import { NextResponse } from 'next/server';
 import { authOptions } from '@/lib/auth';
 
-export default async function GET() {
+export async function GET() {
   const session = await getServerSession(authOptions);
 
   if (!session) {
@@ -11,6 +11,5 @@ export default async function GET() {
     });
   }
 
-  // console.log('GET API', session)
   return NextResponse.json({ authenticated: !!session });
 }
