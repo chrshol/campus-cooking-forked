b3bb3c9b1e9f7a41e8f8d6f19eb123c70b623968
lindseynclement <152480071+lindseynclement@users.noreply.github.com>
2024-12-10 19:33:55 -1000

    navbar update

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -3,7 +3,7 @@ import Recipes from '../../components/Recipes';
 import NavBarSignedin from '@/components/NavBarSignedin';
 import Footer from '@/components/Footer';
 import '../globals.css';
-import './recipepost.css';
+import './recipespage.css';
 import EmailSubscribe from '@/components/EmailSubscribe';
 
 export default function recipes() {
